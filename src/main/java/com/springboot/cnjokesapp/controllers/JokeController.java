9f5fdde6b92ffc8b0c3f7c2d3932ac170b2490c6
cnoriega20 <c79.noriega@gmail.com>
2020-09-14 18:44:05 -0500

    Fixed bean error in service implementation. Change context view reference in Controller. Added template view

--- src/main/java/com/springboot/cnjokesapp/controllers/JokeController.java
+++ src/main/java/com/springboot/cnjokesapp/controllers/JokeController.java
@@ -16,6 +16,6 @@ public class JokeController {
     @RequestMapping({"/", ""})
     public String showJoke(Model model){
         model.addAttribute("joke", jokeService.getJoke());
-        return "chnquote";
+        return "chuckNorrisJokes";
     }
 }
