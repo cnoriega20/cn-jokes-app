60fb2a7e19f50092b8efb161ca35b0f5e94b80df
cnoriega20 <c79.noriega@gmail.com>
2020-09-14 17:30:43 -0500

    First commit: Springboot project structure

---
+++ src/main/java/com/springboot/cnjokesapp/CnJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.cnjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/cnjokesapp/CnJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cnjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
