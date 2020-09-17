d7ed16004ef0b8c60349c2748e4b0686a249615f
cnoriega20 <c79.noriega@gmail.com>
2020-09-17 10:21:46 -0500

    Changed to Java XML Configuration.

--- src/main/java/com/springboot/cnjokesapp/CnJokesAppApplication.java
+++ src/main/java/com/springboot/cnjokesapp/CnJokesAppApplication.java
@@ -2,8 +2,10 @@ package com.springboot.cnjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokes-config.xml")
 public class CnJokesAppApplication {
 
     public static void main(String[] args) {
